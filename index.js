4a67e8126b1f6b1e3e1ea9e514cfc429c29300d7
bonganimasemola <bonganimasemola@icloud.com>
2023-09-28 15:27:16 +0300

    completed DOM modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here!
+
+
+// 1: after index.js is processed, no longer has DOM node 'main#main' 
+
+//document.getElementById('main'); --failed
+//remove('main');                  --failed
+
+const element = document.getElementById('main');
+element.remove(); 
+
+// 2 & 3: after index.js is processed, has a 'newHeader' variable that points to node 'h1#victory' && Make sure you create an <h1> with id 'victory'
+
+/*const h1Element = document.createElement('h1')  --failed
+h1Element.textContent = 'h1#victory' */           
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'h1#victory'; 
+
+// 4: after index.js is processed, has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside 
+
+//document.getElementById('victory').textContent = 'h1#victory BONGANI is the champion'
+
+newHeader.innerHTML = 'h1#victory BONGANI is the champion';
